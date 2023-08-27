59ced58319778d51094b5f17e0e1574042ebf2da
Cory Walker <cory.walker.dev@gmail.com>
2023-08-26 20:51:05 -0700

    write encipher function

--- string.js
+++ string.js
@@ -4,3 +4,36 @@ export function capitalize(string) {
 
   return firstCharCapitalized + restOfWord;
 }
+
+export function reverseString(string) {
+  let reversedString = '';
+  for (let i = string.length - 1; i >= 0; i--) {
+    reversedString = reversedString + string.charAt(i);
+  }
+
+  return reversedString;
+}
+
+export function encipher(string, shift) {
+  let charArray = string.split('');
+  let enchipheredCharArray = charArray.map((char) => {
+    return _encipherChar(char, shift);
+  });
+
+  return enchipheredCharArray.join('');
+}
+
+function _encipherChar(char, shift) {
+  if (/^[A-Za-z]$/.test(char)) {
+    return _shiftChar(char, shift);
+  } else {
+    return char;
+  }
+}
+
+function _shiftChar(char, shift) {
+  let codePoint = char.codePointAt(0);
+  let base = codePoint <= 90 ? 65 : 97;
+  let shiftedCodePoint = ((codePoint + shift - base) % 26) + base;
+  return String.fromCodePoint(shiftedCodePoint);
+}
--- string.test.js
+++ string.test.js
@@ -1,4 +1,4 @@
-import { capitalize } from './string';
+import { capitalize, reverseString, encipher } from './string';
 
 test('capitalize will return a capitalized string', () => {
   expect(capitalize('string')).toBe('String');
@@ -14,4 +14,32 @@ test('capitalize will return the same string if already capitalized', () => {
 
 test('capitalize will return the same string if first string is a number', () => {
   expect(capitalize('1foo')).toBe('1foo');
+});
+
+test('reverseString returns the string reversed', () => {
+  expect(reverseString('string')).toBe('gnirts');
+});
+
+test('reverseString returns the string reversed', () => {
+  expect(reverseString('foo')).toBe('oof');
+});
+
+test('reverseString works with numbers', () => {
+  expect(reverseString('foo1')).toBe('1oof');
+});
+
+test('encipher shifts the chars in the string by 1', () => {
+  expect(encipher('foo', 1)).toBe('gpp');
+});
+
+test('encipher keeps case', () => {
+  expect(encipher('Foo', 2)).toBe('Hqq');
+});
+
+test('encipher does not shift punctuation', () => {
+  expect(encipher('foo!', 1)).toBe('gpp!');
+});
+
+test('encipher wraps correctly', () => {
+  expect(encipher('foo', 27)).toBe('gpp');
 });
